7b276c9c7b34f3bf9a8ff6c16dff57e8b6918353
THEWOLF5 <141071162+THEWOLF5@users.noreply.github.com>
2025-06-02 18:19:03 +0300

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('theme-toggle');
-    toggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('light-mode');
-    });
-});
